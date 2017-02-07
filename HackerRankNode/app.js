1e7982597bd0f00e6b2f0ff5c33a18d87152b937
i5 <know@ledg.es>
2017-02-07 20:48:53 +0100

    initial

---
+++ HackerRankNode/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+    var EOT         = 4;
+    var _input      = "";
+
+    var processData =   (input) =>  {
+        console.log(input);
+    }
+
+    process.stdin.resume        ();
+    process.stdin.setEncoding   ("ascii");
+   
+    process.stdin.on            ("data", (input) => {
+        if ( input.charCodeAt(0) == EOT ) {                                                             //  windows doesn't catch EOT !!
+            processData(_input);
+        }    
+        else _input += input;
+    });
+
+    process.stdin.on            ("end", () => {                                                         //  useless on windows (sess above)
+        processData(_input);
+    });
+
+
